4dbd1917d9f29093f46febc171489aff001abcff
shashank540 <shashank.srivastava5@gmail.com>
2026-08-22 16:44:00 +0530

    copy data operators

--- src/App.jsx
+++ src/App.jsx
@@ -32,24 +32,16 @@ function App() {
         <h2>Core Concepts</h2>
         <ul>
           <CoreConcepts 
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description}
-            image={CORE_CONCEPTS[0].image}
+            {...CORE_CONCEPTS[0]}
           />
           <CoreConcepts 
-            title={CORE_CONCEPTS[1].title}
-            description={CORE_CONCEPTS[1].description}
-            image={CORE_CONCEPTS[1].image}
+            {...CORE_CONCEPTS[1]}
           />
           <CoreConcepts
-            title={CORE_CONCEPTS[2].title}
-            description={CORE_CONCEPTS[2].description}
-            image={CORE_CONCEPTS[2].image}
+            {...CORE_CONCEPTS[2]}
           />
           <CoreConcepts
-            title={CORE_CONCEPTS[3].title}
-            description={CORE_CONCEPTS[3].description}
-            image={CORE_CONCEPTS[3].image}  
+            {...CORE_CONCEPTS[3]}
           />
         </ul>
         </section>
