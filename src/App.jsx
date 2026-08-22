0ab3494e4fa16a8fcb83f09f62e98c8d902da21a
shashank540 <shashank.srivastava5@gmail.com>
2026-08-22 15:56:40 +0530

    fix: first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+const reactDescriptions = ['Fundamental','Core','Crucial'];
+
+function getRandomInt(max){
+  return Math.floor(Math.random()* (max+1));
+}
+
+function Header()
+{
+  const description = reactDescriptions[getRandomInt(2)];
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-conception">
+
+        </section>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+function CoreConcepts(props){
+
+}
+
+export default App;
