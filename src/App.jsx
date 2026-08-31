d320ad7b1fb4b28a834a430782f5e268f7a76334
shashank540 <shashank.srivastava5@gmail.com>
2026-08-31 22:40:03 +0530

    feat: rendering content conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,67 @@
 import { useState } from 'react';
 
-import {CORE_CONCEPTS} from './data.js';
+import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/CoreConcepts/CoreConcepts.jsx';
 import './components/Header/Header.css';
 import './components/CoreConcepts/Coreconcepts.css';
 import TabButton from './components/TabButton.jsx';
-import {EXAMPLES} from './data.js';
+import { EXAMPLES } from './data.js';
 
 function App() {
-  const [tabContent, setTabContent] = useState('components');
+  const [tabContent, setTabContent] = useState();
   function handleClick(selectedButton) {
     setTabContent(selectedButton);
   }
-console.log('App component is rendering');
+  console.log('App component is rendering');
+
+  let tabContentToRender = <p>Please select a topic.</p>;
+
+  if (tabContent) {
+    tabContentToRender = (<div id="tab-content">
+      <h3>{EXAMPLES[tabContent].title}</h3>
+      <p>{EXAMPLES[tabContent].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[tabContent].code}
+        </code>
+      </pre>
+    </div>);
+  }
+
+
   return (
     <div>
       <Header />
       <main>
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcepts 
-            {...CORE_CONCEPTS[0]}
-          />
-          <CoreConcepts 
-            {...CORE_CONCEPTS[1]}
-          />
-          <CoreConcepts
-            {...CORE_CONCEPTS[2]}
-          />
-          <CoreConcepts
-            {...CORE_CONCEPTS[3]}
-          />
-        </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              {...CORE_CONCEPTS[0]}
+            />
+            <CoreConcepts
+              {...CORE_CONCEPTS[1]}
+            />
+            <CoreConcepts
+              {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcepts
+              {...CORE_CONCEPTS[3]}
+            />
+          </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
-          <main>            
+          <main>
             <TabButton onClick={() => handleClick('components')}>Components</TabButton>
             <TabButton onClick={() => handleClick('jsx')}>JSX</TabButton>
             <TabButton onClick={() => handleClick('props')}>Props</TabButton>
             <TabButton onClick={() => handleClick('state')}>State</TabButton>
           </main>
-          <div id="tab-content">
-            <h3>{EXAMPLES[tabContent].title}</h3>
-            <p>{EXAMPLES[tabContent].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[tabContent].code}
-              </code>
-            </pre>
-          </div>
+
+          {tabContentToRender}
+
         </section>
       </main>
     </div>
